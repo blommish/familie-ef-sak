fa2fb4f9c21f4f187f449f2c27911c346be2b5fe
Johan Blomgren <blommish@users.noreply.github.com>
2021-03-24 20:02:11 +0100

    Mye logging

---
+++ src/main/kotlin/no/nav/familie/ef/sak/config/LoggingConfig.kt
@@ -0,0 +1,29 @@
+package no.nav.familie.ef.sak.config
+
+import org.aspectj.lang.ProceedingJoinPoint
+import org.aspectj.lang.annotation.Around
+import org.aspectj.lang.annotation.Aspect
+import org.aspectj.lang.annotation.Pointcut
+import org.slf4j.LoggerFactory
+import org.springframework.context.annotation.Configuration
+
+
+@Configuration
+@Aspect
+class LoggingConfig {
+
+    @Pointcut("execution(public * org.springframework.data.repository.Repository+.*(..)) " +
+              "|| execution(public * no.nav.familie.http.client.AbstractRestClient+.*(..))") fun monitor() {
+    }
+
+    @Around("monitor()") fun profile(pjp: ProceedingJoinPoint): Any? {
+        val logger = LoggerFactory.getLogger(pjp.target.javaClass)
+        val start = System.currentTimeMillis()
+        try {
+            return pjp.proceed()
+        } finally {
+            val elapsedTime = System.currentTimeMillis() - start
+            logger.info("Timer aop - ${pjp.signature.declaringType.simpleName}.${pjp.signature.name} - time:$elapsedTime")
+        }
+    }
+}

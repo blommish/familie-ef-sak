fa5abc3ac7cf7a53e9bc84eb52aa699276c49b0b
eirarset <eirik.arseth@nav.no>
2021-04-12 10:23:59 +0200

    Legg til wrapper

--- src/main/kotlin/no/nav/familie/ef/sak/integration/dto/pdl/PdlGeografiskTilknytning.kt
+++ src/main/kotlin/no/nav/familie/ef/sak/integration/dto/pdl/PdlGeografiskTilknytning.kt
@@ -1,5 +1,7 @@
 package no.nav.familie.ef.sak.integration.dto.pdl
 
+data class PdlHentGeografiskTilknytning(val GeografiskTilknytning: PdlGeografiskTilknytning)
+
 data class PdlGeografiskTilknytning(val gtType: GeografiskTilknytningType,
                                     val gtKommune: String?,
                                     val gtBydel: String?,
